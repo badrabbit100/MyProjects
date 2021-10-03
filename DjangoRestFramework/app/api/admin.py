f4b3fa0f0ace7a9f850c5906eb1c378033539b63
badrabbit100 <80529726+badrabbit100@users.noreply.github.com>
2021-10-03 09:41:17 +0300

    Delete DjangoRestFramework directory

--- DjangoRestFramework/app/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Album, Tag, Photo
-
-
-admin.site.register(Album)
-admin.site.register(Tag)
-admin.site.register(Photo)
--- DjangoRestFramework/app/api/test.py
+++
@@ -1,4 +0,0 @@
-import datetime
-s = '2021-09-22T06:57:53.481550Z'
-
-print(datetime.datetime.now() + datetime.timedelta(days=2))
